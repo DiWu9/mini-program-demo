e3027616ca85cf6e131bbe4e7f611ba41864c413
Woody <14566@wisdri.com>
2024-03-04 23:17:52 +0800

    styling

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,7 +17,7 @@ Page({
         avatar: 'https://th.bing.com/th/id/R.e3693e6cc5e44d11d1d7a55e4f05bced?rik=ZA8cULnAuXYO2A&riu=http%3a%2f%2fgetdrawings.com%2fvectors%2f80s-vector-30.jpg&ehk=4bfR0vskLRYw8%2bPsl1nqOkh%2fZx5zRzuTSD25FXPF53c%3d&risl=&pid=ImgRaw&r=0',
         nickname: 'username_1',
         address: '华中师范大学',
-        date: '2023年10月26日 13:26',
+        date: '2024年01月26日 09:26',
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
         images: [
           'https://cdn.naturettl.com/wp-content/uploads/2019/08/30182353/how-to-photograph-black-and-white-landscapes-5-800x800.jpg',
@@ -33,6 +33,21 @@ Page({
         likes: 55,
         comments: 10,
         shares: 20,
+      },
+      {
+        avatar: 'https://th.bing.com/th/id/R.4768c70fefa9f1800586f1e8eb45e28c?rik=oQKTAhqpEr5ajA&riu=http%3a%2f%2ffarm2.static.flickr.com%2f1094%2f5120736582_a1f8903266_b.jpg&ehk=FMR%2bmrXPChp67CCe3p9%2bDB4Xnpu1u1UvclShP79FxDA%3d&risl=&pid=ImgRaw&r=0',
+        nickname: 'username_2',
+        address: '武汉大学',
+        date: '2023年10月26日 13:26',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+        images: [
+          'https://png.pngtree.com/thumb_back/fw800/back_our/20190619/ourmid/pngtree-neon-color-light-party-background-material-image_137760.jpg',
+          'https://www.rxwallpaper.site/wp-content/uploads/space-free-hd-top-most-downloaded-wallpapers-page-6-800x800.jpg',
+          'https://petapixel.com/assets/uploads/2021/12/ESO-rogue-planets-petapixel-3-800x800.jpg'
+        ],
+        likes: 100,
+        comments: 55,
+        shares: 60,
       },
     ]
   },
